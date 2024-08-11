0391772ed128d5d2de33c750e46fbdf496b3e9ac
Ricardo Alonso <ricardoaloof@gmail.com>
2024-08-11 17:18:23 +0200

    Solution to problem # On

--- Arrays & Strings/Find Closest Number to Zero.cpp
+++ Arrays & Strings/Find Closest Number to Zero.cpp
@@ -1,1 +1,14 @@
+class Solution {
+public:
+    int findClosestNumber(vector<int>& nums) {
+        int result = {nums[0]};
 
+        for(int iii = 1; iii < nums.size();iii++)
+        {
+            if(abs(nums[iii]) < abs(result) || (abs(nums[iii]) == abs(result) && nums[iii] > result))
+                result = nums[iii];
+        }
+        
+        return result;
+    }
+};
