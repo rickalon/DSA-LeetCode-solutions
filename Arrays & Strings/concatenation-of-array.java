c1bfcadcb9e5a6fd055e5bad1b9cde2e6789f62a
Ricardo Alonso <ricardoaloof@gmail.com>
2024-10-17 23:02:09 +0200

    sol

---
+++ Arrays & Strings/concatenation-of-array.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] answer = new int[nums.length*2];
+        for(int i = 0 ; i < answer.length; i++){
+            answer[i] = nums[i%nums.length];
+        }
+        return answer;
+    }
+}
