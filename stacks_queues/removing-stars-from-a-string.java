e59023984822db7ed1a24a72cff215503b0677a2
Ricardo Alonso <ricardoaloof@gmail.com>
2024-11-04 12:16:19 +0100

    sol improve

--- stacks_queues/removing-stars-from-a-string.java
+++ stacks_queues/removing-stars-from-a-string.java
@@ -16,3 +16,21 @@ class Solution {
     return str.toString();  
     }
 }
+
+//improvement
+class Solution {
+    public String removeStars(String s) {
+    StringBuilder str = new StringBuilder();
+    for(char c : s.toCharArray())
+    {
+        if(c == '*')
+        {
+            str.delete(str.length()-1,str.length());
+        }else
+        {
+            str.append(c);
+        }
+    }
+    return str.toString();  
+    }
+}
