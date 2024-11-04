70cbfca8dcea3a4dc16831c304aea925f7e755fe
Ricardo Alonso <ricardoaloof@gmail.com>
2024-11-04 18:11:56 +0100

    sol improvement

--- linked_list/happy-number.java
+++ linked_list/happy-number.java
@@ -18,3 +18,37 @@ class Solution {
     return true;   
     }
 }
+//Floyd optimization, constant space
+class Solution {
+    public boolean isHappy(int n) {
+    int slow = n;
+    int fast = n;
+    boolean prim = true;
+    while(slow!=fast || prim)
+    {
+        prim = false;
+
+        slow = square(slow);
+        
+        fast = square(fast);
+        fast = square(fast);
+        
+        if(slow == 1 || fast ==1){
+            return true;
+        }
+    }
+    return false;   
+    }
+
+    public int square(int n)
+    {
+        int sum = 0;
+        while(n > 0){
+        int aux = n % 10;
+        sum+= aux*aux;
+        n /= 10;
+        }
+        return sum;
+    }
+}
+
