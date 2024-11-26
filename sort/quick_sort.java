89a4824f9a83757f18ef1c60f8f550e846d9a1aa
Ricardo Alonso <ricardoaloof@gmail.com>
2024-11-26 13:29:00 +0100

    sol

---
+++ sort/quick_sort.java
@@ -0,0 +1,34 @@
+ public static void main(String[] args){
+        long start = System.currentTimeMillis();
+        int[] array = {10,2,13,20,1};
+        quickSort(array);
+        System.out.println(Arrays.toString(array));
+        System.out.printf("Times to complete: %d ms.",System.currentTimeMillis()-start);
+    }
+    public static void quickSort(int[] array){
+        quickSortCore(array,0,array.length-1);
+    }
+    public static void quickSortCore(int[] array,int left,int right){
+        if(left < right){
+        //right element pivot
+        int mid = LomutoPartition(array,right,left,right);
+        quickSortCore(array,left,mid-1);
+        quickSortCore(array,mid+1,right);
+        }
+    }
+    
+    public static int LomutoPartition(int[] array,int pivot,int left,int right){
+        int low = left-1;
+        for(int i = left;i<=right;i++){
+            if(array[i] < array[pivot]){
+                low++;
+                int aux = array[i];
+                array[i] = array[low];
+                array[low] = aux;
+            }
+        }
+    int aux = array[low+1];
+    array[low+1] = array[pivot];
+    array[pivot] = aux;
+    return low+1;
+    }
