f6df60f622587f5683ba79ac544e8f16127bc00a
Ricardo Alonso <ricardoaloof@gmail.com>
2024-12-13 17:07:51 +0100

    sol

---
+++ Arrays & Strings/decode-string.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String decodeString(String s) {
+        int[] index = {0};
+        return decodeStringV(s,index);
+    }
+
+    public String decodeStringV(String s,int[] index) {
+        String str = "";
+        int times = 0;
+        for(; index[0]< s.length();index[0]++)
+        {
+            char c = s.charAt(index[0]);
+            if(c == '['){
+                index[0]++;
+                String aux = decodeStringV(s,index);
+                for(int j = 0 ; j < times ; j++)
+                {
+                    str += aux;
+                }
+            }else if(c==']'){
+                return str;
+            }else if(Character.isDigit(c)){
+                int start = index[0];
+                while(Character.isDigit(s.charAt(index[0]+1))){
+                    index[0]++;
+                }
+                times = Integer.valueOf(s.substring(start,index[0]+1));
+            }else{
+                str += c;
+            }
+        }
+        return str;
+    }
+}
