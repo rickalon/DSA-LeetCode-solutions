1a627538e8f7f1c27589395426ef21917a1a2af9
Ricardo Alonso <ricardoaloof@gmail.com>
2024-12-13 17:10:31 +0100

    sol better performance 0ms

--- Arrays & Strings/decode-string.java
+++ Arrays & Strings/decode-string.java
@@ -32,3 +32,39 @@ class Solution {
         return str;
     }
 }
+
+
+class Solution {
+    public String decodeString(String s) {
+        int[] index = {0};
+        return decodeStringV(s,index);
+    }
+
+    public String decodeStringV(String s,int[] index) {
+        StringBuilder str = new StringBuilder();
+        int times = 0;
+        for(; index[0]< s.length();index[0]++)
+        {
+            char c = s.charAt(index[0]);
+            if(c == '['){
+                index[0]++;
+                StringBuilder aux = new StringBuilder(decodeStringV(s,index));
+                for(int j = 0 ; j < times ; j++)
+                {
+                    str.append(aux);
+                }
+            }else if(c==']'){
+                return str.toString();
+            }else if(Character.isDigit(c)){
+                int start = index[0];
+                while(Character.isDigit(s.charAt(index[0]+1))){
+                    index[0]++;
+                }
+                times = Integer.valueOf(s.substring(start,index[0]+1));
+            }else{
+                str.append(c);
+            }
+        }
+        return str.toString();
+    }
+}
