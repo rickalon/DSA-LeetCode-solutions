ad5f81261af2f365233c9be1e1ffe867830f83c5
Ricardo Alonso <ricardoaloof@gmail.com>
2025-02-11 15:22:46 +0100

    improve performance

--- sliding_window/remove-all-occurrences-of-a-substring.java
+++ sliding_window/remove-all-occurrences-of-a-substring.java
@@ -48,3 +48,14 @@ class Solution {
     return new String(ans);    
     }
 }
+
+
+//better performnace sol
+class Solution {
+    public String removeOccurrences(String s, String part) {
+        StringBuilder str = new StringBuilder(s);
+        while(str.indexOf(part)!=-1)
+            str.delete(str.indexOf(part),str.indexOf(part) + part.length());
+        return str.toString();
+    }
+}
